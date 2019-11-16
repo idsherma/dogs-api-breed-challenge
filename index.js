fe679dac5586559214180ebe2bda569ce21b11bf
Ivory Sherman <ivorysherman@Ivorys-MacBook-Pro.local>
2019-11-15 20:45:46 -0500

    WIP

--- index.js
+++ index.js
@@ -2,100 +2,34 @@
     scs(window.jQuery, window, document);
     }(function($, window, document) {
 
-        // function shoppingList() {
-
-        //     $("#js-shopping-list-form").submit(function(e) {
-        //       e.preventDefault();
-
-        //         let textItem = $(this).find('input[name="shopping-list-entry"]').val();
-
-        //         if (textItem !== "") {	
-        //             $('.shopping-list').append((`<li>
-        //             <span class="shopping-item">${textItem}</span>
-        //             <div class="shopping-item-controls">
-        //                 <button class="shopping-item-toggle">
-        //                 <span class="button-label">check</span>
-        //                 </button>
-        //                 <button class="shopping-item-delete">
-        //                 <span class="button-label">delete</span>
-        //                 </button>
-        //             </div>
-        //             </li>`));
-    
-        //           $('#shopping-list-entry').val('');
-        //         } 
-
-        //     });
-
-        //     $('.shopping-list').on('click', '.shopping-item-toggle', function() {
-        //         $(this).parent().prev().toggleClass('shopping-item__checked');
-        //     });
-            
-        //     $('.shopping-list').on('click','.shopping-item-delete', function() {
-        //         $(this).closest('li').remove();
-        //     });
-        // }
-
-        // shoppingList();
-
         'use strict';
 
-        function createNode(element) {
-            return document.createElement(element);
-        }
-      
-        function append(parent, el) {
-          return parent.appendChild(el);
-        }
-      
-        const ul = document.getElementById('authors');
+        function getDogImage(userBreedEntry) {
+            let breedParameter = userBreedEntry;
 
-        function getDogImage(num) {
-            let numParameter = num;
-            const URL = 'https://dog.ceo/api/breeds/image/random/';
-
-            const newURL = URL + numParameter;
-            console.log("number of dogs requested was" + " " + `${numParameter}`);
+            let newURL = `https://dog.ceo/api/breed/${userBreedEntry}/images/random`;
+            //console.log(test);
 
             fetch(newURL)
-                .then(response => {
-                    return response.json()
-                })
-                .then(data => {
-                    // Work with JSON data here
-                    //console.log(data);
+                .then(response => response.json())
+                .then(data => 
+                    displayResults(data))
+        }
 
-                    let dogData = data.message;
+        function displayResults(data) {
+            console.log(data);
 
-                    function accessDogData() {
-                        let dogArr = [];
+            $('#dog-list').append(``);
 
-                        dogData.forEach(function (dogData, index) {
-                            console.log(dogData);
-                            dogArr.push(
-                                `<img src=${dogData}>`
-                            );
-                        });
-
-                        return `${dogArr}`;
-                    } 
-
-                    function render() {
-                        let dogPhotoString = accessDogData();
-                        $('#dog-list').html(dogPhotoString);
-                    }
-    
-                    render();
-                    
-                })
         }
 
         function watchForm() {
             $('form').submit(event => {
                 event.preventDefault();
 
-               let num = $('#dogs').val();
-               getDogImage(num);
+               let userBreedEntry = $('#breed-list-entry').val();
+               console.log(userBreedEntry);
+               getDogImage(userBreedEntry);
 
             });
         }
